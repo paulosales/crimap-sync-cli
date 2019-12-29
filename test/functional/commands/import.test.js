6325f571b9a71b446511e3dca23d9d3da205b71e
Paulo Rogério Sales Santos <paulosales@gmail.com>
2019-12-29 18:50:44 -0300

    - fix functional test

--- test/functional/commands/import.test.js
+++ test/functional/commands/import.test.js
@@ -7,26 +7,28 @@
 
 /* eslint-env mocha, node */
 const assert = require('chai').assert;
+const { config } = require('../../../src/config');
 const crimeSync = require('../test-utils/crime-sync-runner');
 const eraseImports = require('../test-utils/erase-imports');
 
 describe('[functional] crime-sync import command', () => {
 
-	before(eraseImports);
+	before(async () => {
+		await config.set('serviceUrl', 'http://127.0.0.1:4000/');
+		await config.save();
+		await eraseImports();
+	});
 
 	context('with a informed pdf file url', () => {
-
 		it('should start import process.', async () => {
 			const ret = await crimeSync(['import', 'http://domain.com/teste.pdf']);
 			assert.equal(ret.stderr, '', 'shouldn\'t have error. Maybe there are some imports.');
 			const lines = ret.stdout.split('\n');
 			assert.equal(lines[0], 'File \'teste.pdf\' imported.');
 		});
-
 	});
 
 	context('with a pdf file url already imported', () => {
-
 		it('should start import process.', async () => {
 			const inserted = await crimeSync(['import', 'http://domain.com/teste2.pdf']);
 			assert.notEqual(inserted.stdout, '', 'the import stdout should have a output text.');
@@ -39,7 +41,6 @@ describe('[functional] crime-sync import command', () => {
 			assert.isNotNull(ret.stderr);
 			assert.equal(ret.stderr, `The file http://domain.com/teste2.pdf is already imported with the ID '${insertedId}'.\n`);			
 		});
-
 	});
 	
   context('with no pdf file url', () => {
@@ -47,6 +48,5 @@ describe('[functional] crime-sync import command', () => {
 			const ret = await crimeSync(['import']);
 			assert.equal(ret.stderr, "error: missing required argument 'pdf-url'\n");
 		});
-
 	});
 });
